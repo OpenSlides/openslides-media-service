86ad112b0746839bc4a8618560290bc00f4c5e53
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-07-01 11:37:50 +0200

    Makefile Rework

---
+++ dev/run-lint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Executes all linters. Should errors occur, CATCH will be set to 1, causing an erroneous exit code.
+
+echo "########################################################################"
+echo "###################### Run Linters #####################################"
+echo "########################################################################"
+
+# Parameters
+while getopts "lbp" FLAG; do
+    case "${FLAG}" in
+    l) LOCAL=true ;;
+    b) BUILD=true ;;
+    p) PERSIST_CONTAINERS=true ;;
+    *) echo "Can't parse flag ${FLAG}" && break ;;
+    esac
+done
+
+# Setup
+IMAGE_TAG=openslides-media-tests
+CATCH=0
+DC="docker compose -f docker-compose.test.yml"
+
+# Optionally build image
+if [ -n "$BUILD" ]
+then
+    if [ "$(docker images -q openslides-media-dev)" = "" ]; then make build-dev || CATCH=1; fi
+    if [ "$(docker images -q openslides-media-tests)" = "" ]; then make build-test || CATCH=1; fi
+    docker build . -f tests/dummy_autoupdate/Dockerfile.dummy_autoupdate --tag openslides-media-dummy-autoupdate || CATCH=1
+
+	eval "$DC up -d" || CATCH=1
+	eval '$DC -T tests wait-for-it "media:9006"' || CATCH=1
+fi
+
+# Execution
+if [ -z "$LOCAL" ]
+then
+    # Container Mode
+    docker run -d -t --name autoupdate-test ${IMAGE_TAG} || CATCH=1
+
+    eval "$DC exec -T tests black --check --diff src/ tests/" || CATCH=1
+    eval "$DC exec -T tests isort --check-only --diff src/ tests/" || CATCH=1
+    eval "$DC exec -T tests tests flake8 src/ tests/" || CATCH=1
+
+else
+    # Local Mode
+    black --diff src/ tests/
+    isort --diff src/ tests/
+    flake8 src/ tests/
+fi
+
+if [ -z "$PERSIST_CONTAINERS" ] && [ -n "$BUILD" ]; then docker stop autoupdate-test && docker rm autoupdate-test || CATCH=1; fi
+
+exit $CATCH
